fa2bc88f0d3e99798c40bf6a710e8f3372f5d639
tmhSept <143030816+tmhSept@users.noreply.github.com>
2023-08-25 13:54:57 -0600

    Update streamlit_app.py
    dfzdfv

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice")
