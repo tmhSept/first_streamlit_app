b3f61aa0f29a0052e7123829cc8149bf2646d6fa
tmhSept <143030816+tmhSept@users.noreply.github.com>
2023-08-29 13:03:08 -0600

    Update streamlit_app.py
    ljkhlkhj

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
-
 streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -15,7 +14,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -41,11 +39,9 @@ try:
 except URLError as e:
     streamlit.error()
   
-streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
