f06c4f450afe4b88fbe98383c23d68b63c4ebbbc
tmhSept <143030816+tmhSept@users.noreply.github.com>
2023-08-29 09:54:58 -0600

    Update streamlit_app.py
    MORE

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
-user = "trinahughes"
+user = "TRINAHUGHES"
 password = "2023Sept"
 account = "KQ73646"
 warehouse = "pc_rivery_wh" 
